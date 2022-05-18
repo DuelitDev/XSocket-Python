35d5a5c3169ec71a893e7c78b9c2cfcad4788461
DuelitDev <jyoon07dev@gmail.com>
2022-05-18 23:47:57 +0900

    add __init__

--- XSocket/base/handle.py
+++ XSocket/base/handle.py
@@ -1,4 +1,6 @@
 import typing
+import socket
+import asyncio
 from abc import ABCMeta, abstractmethod
 from address import *
 from ..protocol import ProtocolType
@@ -8,6 +10,14 @@ __all__ = ["Handle"]
 
 
 class Handle(metaclass=ABCMeta):
+    def __init__(self, *args, **kwargs) -> None:
+        self._socket: socket.socket
+        self._protocol_type: ProtocolType
+        self._event_loop: asyncio.BaseEventLoop
+        self._available: bool = False
+        self._connected: bool = False
+        self._closed: bool = False
+
     @property
     @abstractmethod
     def available(self) -> bool:
