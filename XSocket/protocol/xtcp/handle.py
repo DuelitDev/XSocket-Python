e1a14b3a2e353728f8e68b773d9dbb56420e296e
DuelitDev <jyoon07dev@gmail.com>
2022-05-26 08:22:09 +0900

    edit unpack/receive methods

--- XSocket/protocol/xtcp/handle.py
+++ XSocket/protocol/xtcp/handle.py
@@ -1,5 +1,5 @@
 import asyncio
-from struct import pack, unpack
+import struct
 import typing
 import socket
 import enum
@@ -43,10 +43,9 @@ class XTCPHandle(Handle):
     def protocol_type(self) -> ProtocolType:
         return ProtocolType.Xtcp
 
-# TODO: refactoring codes
+    # TODO: refactoring codes
     @staticmethod
-    def pack(data: typing.Union[bytes, bytearray],
-             opcode: OPCode = OPCode.Continuation,
+    def pack(data: bytearray, opcode: OPCode = OPCode.Continuation,
              *args, **kwargs) -> typing.Generator[bytearray]:
         fin, con = 128, opcode.Continuation
         payload_length = (125, 65535)
@@ -54,29 +53,60 @@ class XTCPHandle(Handle):
             header = bytearray([fin | opcode, len(data)])
             yield header + data
         elif len(data) <= payload_length[1]:
-            header = bytearray([fin | opcode, 126, *pack("!H", len(data))])
+            header = bytearray([
+                fin | opcode, payload_length[0] + 1,
+                *struct.pack("!H", len(data))])
             yield header + data
         else:
             for index in range(0, len(data), payload_length[1]):
                 segment = data[index:index + payload_length[1]]
                 header = bytearray([
-                    opcode if not index else con,
-                    126, *pack("!H", len(segment))])
+                    opcode if not index else con, payload_length[0] + 1,
+                    *struct.pack("!H", len(segment))])
                 yield header + segment
             yield bytearray([fin | con, 0])  # finish packet
 
     @staticmethod
-    def unpack(packets: typing.List[typing.Union[bytes, bytearray]],
-               opcode: OPCode = OPCode.Continuation,
-               *args, **kwargs) -> typing.Generator[int, bool]:
-        pass
+    def unpack(packets: typing.List[bytearray], *args, **kwargs
+               ) -> typing.Generator[int, typing.Tuple[OPCode, bytearray]]:
+        for packet in packets:
+            payload_length = (125, 65535)
+            yield 2
+            fin = (255 & packet[0]) >> 7
+            op = OPCode(15 & packet[0])
+            size = 127 & packet[1]
+            if size == payload_length[0] + 1:
+                yield 2
+                size = struct.unpack("!H", *packet[2:])
+            yield size
+            yield op, packet[2 + size <= payload_length[1] * 2:]
+            if fin:
+                break
 
-    async def send(self, data: typing.Union[bytes, bytearray]) -> None:
-        for packet in self.pack(data, OPCode.Data):
+    async def send(self, data: typing.Union[bytes, bytearray],
+                   opcode: OPCode = OPCode.Data) -> None:
+        for packet in self.pack(bytearray(data), opcode):
             await self._event_loop.sock_sendall(self._socket, packet)
 
-    async def receive(self) -> typing.Any:
-        pass
+    async def receive(self) -> bytes:
+        packets = []
+        opcode = None
+        temp = [bytearray()]
+        for packet in self.unpack(temp):
+            if isinstance(packet, int):
+                recv = []
+                while len(recv) != packet:
+                    recv = await self._event_loop.sock_recv(
+                        self._socket, packet - len(recv))
+                temp[-1].extend(recv)
+            elif isinstance(packet, tuple):
+                if not opcode:
+                    opcode = packet[0]
+                if opcode == OPCode.Ping:
+                    await self.send(b"", OPCode.Pong)
+                elif opcode == OPCode.Data:
+                    packets.append(packet[1])
+        return b"".join(packets)
 
     def close(self) -> None:
         self._socket.close()
