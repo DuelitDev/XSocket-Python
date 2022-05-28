f9c33d46a7c288de2644564cdba444b67b4d68ae
DuelitDev <jyoon07dev@gmail.com>
2022-05-28 23:30:09 +0900

    edit ProtocolType

--- XSocket/protocol/protocol.py
+++ XSocket/protocol/protocol.py
@@ -522,7 +522,7 @@ class ProtocolType(Enum):
     """
     Specifies the protocol scheme.
     """
-    Unspecified = (0, 0)
+    Unspecified = 0
     """Unspecified protocol."""
-    Xtcp = (1, 1)
+    Xtcp = 1
     """Extend TCP protocol."""
