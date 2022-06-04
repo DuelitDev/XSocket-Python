539d333b07a77694b7b0be60b43337639ab4b59c
DuelitDev <jyoon07dev@gmail.com>
2022-06-04 21:57:31 +0900

    add docstring for core/listener

--- XSocket/core/listener.py
+++ XSocket/core/listener.py
@@ -522,6 +522,9 @@ __all__ = ["Listener"]
 
 
 class Listener(metaclass=ABCMeta):
+    """
+    Listens for connections from network clients.
+    """
     def __init__(self, *args, **kwargs) -> None:
         self._socket: typing.Union[socket.socket, None] = None
         self._address: typing.Union[AddressInfo, None] = None
@@ -531,39 +534,82 @@ class Listener(metaclass=ABCMeta):
 
     @property
     def is_running(self) -> bool:
+        """
+        Gets a value indicating whether
+        the Socket for a Listener is running.
+
+        :return: bool
+        """
         return self._running
 
     @property
     def closed(self) -> bool:
+        """
+        Gets a value indicating whether
+        the Socket for a Listener has been closed.
+
+        :return: bool
+        """
         return self._closed
 
     @property
     @abstractmethod
     def local_address(self) -> AddressInfo:
+        """
+        Gets the local endpoint.
+
+        :return: AddressInfo
+        """
         pass
 
     @property
     @abstractmethod
     def address_family(self) -> AddressFamily:
+        """
+        Gets the address family of the Socket.
+
+        :return: AddressFamily
+        """
         pass
 
     @property
     @abstractmethod
     def protocol_type(self) -> ProtocolType:
+        """
+        Gets the protocol type of the Socket.
+
+        :return: ProtocolType
+        """
         pass
 
     @abstractmethod
     def run(self) -> None:
+        """
+        Starts listening for incoming connection requests.
+        """
         pass
 
     @abstractmethod
     async def connect(self) -> Handle:
+        """
+        Establishes a connection to a remote host.
+
+        :return: Handle
+        """
         pass
 
     @abstractmethod
     async def accept(self) -> Handle:
+        """
+        Creates a new Handle for a newly created connection.
+
+        :return: Handle
+        """
         pass
 
     @abstractmethod
     def close(self) -> None:
+        """
+        Closes the listener.
+        """
         pass
