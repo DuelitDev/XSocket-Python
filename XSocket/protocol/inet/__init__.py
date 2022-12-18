f1c6bee433d30a780d44b028f60d8be98e554e75
DuelitDev <100027958+DuelitDev@users.noreply.github.com>
2022-12-18 15:10:10 +0900

    code refactoring

--- XSocket/protocol/inet/__init__.py
+++ XSocket/protocol/inet/__init__.py
@@ -3,3 +3,6 @@
 # Copyright 2022. DuelitDev all rights reserved.
 #
 # This Library is distributed under the LGPL-2.1 License.
+
+from XSocket.protocol.inet.net import *
+from XSocket.protocol.inet.xtcp import *
